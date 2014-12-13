322eba297893f798bd513438d8e3b9652c334ec3
Shannon <shannon@mada.us>
2014-12-13 15:41:11 -0500

    Added a Calculation
    Reviewed directions and added a calculation of number of supplements to
    order.

--- server.R
+++ server.R
@@ -2,34 +2,36 @@ library(shiny)
 library(ggplot2)
 oj <- read.table("data/oj_pop")
 acid<-read.table("data/acid_pop")
+roundUp<-function(x) ceiling(max(x)/1)
 
 shinyServer(
+            
         function(input, output) {
-                             
-             
-                output$text1 <- renderText({ 
-                        paste("You have selected ", input$vehicle)
+                 
+                output$text1<-renderText({
+                        paste("You have selected ", input$hamsters, "hamster(s) and ",input$vehicle, "
+                        as the delivery vehicle at the dosage of", input$dosage, "mg")
                 })
+                output$text2<-renderText({
+                        paste("You will need to order the following number of our 10 mg bottles: " ,
+                              (roundUp((input$hamsters * input$dosage) / 10)))
+                         })
                 
-                output$text2 <- renderText({ 
-                        paste("Your dosage in milligrams ",input$dosage)
-                })
-                
-                output$plot<-renderPlot({
-                if (input$vehicle=="Orange Juice") {                            
-                        p<- ggplot(oj, 
-                                   aes(x = dose, y = len, group=id)
-                        ) + geom_line(aes(color = factor(id))) +labs(title="Orange Juice Clinical Trial")  
-                       p + geom_vline(xintercept=input$dosage) }
-                else 
-                { p<-ggplot(acid, 
-                        aes(x = dose, y = len, group=id)
-                        ) +geom_line(aes(color = factor(id))) +labs(title="Ascorbic Acid Trial")
-                  p + geom_vline(xintercept=input$dosage) }
+                output$plot<-renderPlot({ 
+                        if (input$vehicle=="Orange Juice") {
+                                p<-ggplot(oj, 
+                               aes(x = dose, y = len, group=id)
+                                ) +geom_line(aes(color = factor(id))) +labs(title="Orange Juice Trial - 10 Hamster Factor ID's")
+                                p + geom_vline(xintercept=input$dosage)
+                        }
+                        else{
+                                p<-ggplot(acid, 
+                                aes(x = dose, y = len, group=id)
+                                ) +geom_line(aes(color = factor(id))) +labs(title="Abscorbic Acid Trial - 10 Hamster Factor ID's")
+                                p + geom_vline(xintercept=input$dosage)
+                        }
                 })
         })
-                        
-                    
-                        
-                           
-
+                
+                
+ 
--- ui.R
+++ ui.R
@@ -2,8 +2,10 @@ library(shiny)
 library(datasets)
 oj <- read.table("data/oj_pop")
 acid<-read.table("data/acid_pop")
+roundUp<-function(x) ceiling(max(x)/1)
 
 # ui.R
+
 
 shinyUI(fluidPage(
         titlePanel("Longer Shinier Teeth"),
@@ -11,10 +13,11 @@ shinyUI(fluidPage(
                 sidebarPanel(
                         h4("How to Use this Application"),
                         p("Using a Vitamin C supplement will increase the size and beauty of your rodent's teeth.  
-                          Choose the delivery vehicle using the radio button and the slider bar to specify dosage.
-                          The chart line will highlight the range of possible effect on your pet's teeth as demonstrated by our advanced
+                          Choose the delivery vehicle using the radio button, the slider bar to specify dosage.
+                          and enter the number of hamsters you own.  The chart line will highlight the range of possible effect on your pet's teeth as demonstrated by our advanced
                           clinical trials. As you move the slider, a vertical line will move along with your selection.  
-                          Experiment on your own hamster and see if your results fall into the same range as ours!
+                          You'll also see that we calculate how many bottles of supplement you'll need to order 
+                          to participate.  Experiment on your own hamster and see if your results fall into the same range as ours!
                           Send your results to:"),
                         h5("HamstersWelfare@gmail.com"),
                         p("and we'll add them to our data set."),
@@ -28,11 +31,15 @@ shinyUI(fluidPage(
                                     label = "Dosage in mg:",
                                     min = .5, max = 2, value = c(.5),
                                     ),
+                       numericInput("hamsters", 
+                                    label = "Enter # of hamsters to treat", 
+                                            min=1, max=25, value = 1),
                         br(),
                         br(),
                         img(src = "hamster.jpg", height = 35, width = 35),
                         "Study is sponsored by ", 
                         span("Welfare for Hamsters, Inc.", style = "color:green")
+                        
                 ),
                 mainPanel(
                         h3("Predicting Results"),
@@ -43,6 +50,8 @@ shinyUI(fluidPage(
                         textOutput("text1"),
                         textOutput("text2"),
                         plotOutput("plot")
+                        
+                        
                 )
         )
 ))
